4fdeb8735de2717c30a62796bb56a5a0f37f29ab
ridwan-p <rrridwanp@gmail.com>
2016-04-01 13:40:16 +0700

    add code

---
+++ src/com/rpmaps/SignupActivity.java
@@ -0,0 +1,20 @@
+package com.rpmaps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignupActivity extends Activity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_signup);
+
+	}
+
+	public void btnSignUpOnclick(View view) {
+
+	}
+}
