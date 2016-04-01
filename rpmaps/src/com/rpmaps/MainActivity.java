5eed441c7723adf65b1c082968fdb745ec389eba
ridwan-p <rrridwanp@gmail.com>
2016-04-01 13:06:29 +0700

    code rpmas

---
+++ rpmaps/src/com/rpmaps/MainActivity.java
@@ -0,0 +1,23 @@
+package com.rpmaps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+	public void btnLoginOnClick(View view) {
+
+	}
+}
