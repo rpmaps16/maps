224a255d484500aa17613a1d431854830ddb5c90
suryaramadhon <suryaramadhon1995@gmail.com>
2016-04-23 17:28:51 +0700

    volley

--- app/src/main/java/json/data/OpenWeather.java
+++ app/src/main/java/json/data/OpenWeather.java
@@ -3,11 +3,11 @@ package json.data;
 /**
  * Created by suryaramadhon on 4/23/16.
  */
-public class GoogleWeather extends JsonData{
+public class OpenWeather extends JsonData{
     private static final String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
 
-    public GoogleWeather(String url) {
+    public OpenWeather(String url) {
         super(url);
     }
-    public GoogleWeather(){super(BASE_URL);}
+    public OpenWeather(){super(BASE_URL);}
 }
